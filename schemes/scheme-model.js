5851bef65f4f31838c5ab6fac3367bbcad26bcde
sooshe <sooshe@sooshebot.com>
2020-02-19 16:31:30 -0600

    update findsteps(id) for clarity

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -12,17 +12,17 @@ module.exports = {
 
 function find() {
   return db("schemes");
-};
+}
 
 function findById(id) {
   return db("schemes")
     .where({ id })
     .first();
-};
+}
 
 function findSteps(id) {
   // what it looks like in sqlite3
-  // select [s].id, [s].step_number, [s].instructions, [s].scheme_id
+  // select [s].id, [sc].scheme_name, [s].step_number, [s].instructions
   // from steps as s
   // join schemes as sc
   // on [s].scheme_id = [sc].id
@@ -30,44 +30,49 @@ function findSteps(id) {
   return db("steps as s")
     .select(
       "s.id as step_id",
+      "sc.scheme_name as scheme_name",
       "s.step_number as step number",
-      "s.instructions as instructions",
-      "sc.id as scheme_id"
+      "s.instructions as instructions"
     )
     .join("schemes as sc", "s.scheme_id", "=", "sc.id")
     .where("scheme_id", id);
-};
+}
 
 function add(scheme) {
-    return db("schemes")
+  return db("schemes")
     .first()
     .insert(scheme);
-};
+}
 
 function addStep(step, scheme_id) {
-    return db("steps as s")
-    .select("sc.id as scheme_id",
-    "s.step_number as step_number",
-    "s.instructions as instructions"
+  return db("steps as s")
+    .select(
+      "sc.id as scheme_id",
+      "s.step_number as step_number",
+      "s.instructions as instructions"
     )
     .insert(step)
-    .join("steps as s", 
-    "s.step_number", 
-    "s.instructions",
-    "sc.id", "=", "s.scheme_id" )
+    .join(
+      "steps as s",
+      "s.step_number",
+      "s.instructions",
+      "sc.id",
+      "=",
+      "s.scheme_id"
+    )
     .where("scheme_id", scheme_id)
     .then(count => findById(id));
-};
+}
 
 function update(changes, id) {
-    return db("schemes", "id")
+  return db("schemes", "id")
     .where({ id })
     .update(changes, "*")
     .then(count => findById(id));
-};
+}
 
 function remove(id) {
-    return db("schemes")
+  return db("schemes")
     .where({ id })
     .del();
-};
+}
